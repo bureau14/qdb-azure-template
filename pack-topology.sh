25d90da702ad8588f155b2648ec43ce180d31ba1
Benoit Blanchon <benoit@quasardb.net>
2016-05-13 15:02:53 +0200

    Allow to reuse an existing storage account

--- pack-topology.sh
+++ pack-topology.sh
@@ -5,16 +5,17 @@ ZIPFILE="topology-$VERSION.zip"
 
 rm -f $ZIPFILE
 
-7z a $ZIPFILE                 \
-    availabilitySet.json      \
-    createUiDefinition.json   \
-    mainTemplate.json         \
-    networkSecurityGroup.json \
-    publicIpAddress.json      \
-    quasardbNode.json         \
-    quasardbNode.setup.sh     \
-    quasardbTestbox.json      \
-    quasardbTestbox.motd.txt  \
-    quasardbTestbox.setup.sh  \
-    storageAccount.json       \
+7z a $ZIPFILE                    \
+    availabilitySet.json         \
+    createUiDefinition.json      \
+    mainTemplate.json            \
+    networkSecurityGroup.json    \
+    publicIpAddress.json         \
+    quasardbNode.json            \
+    quasardbNode.setup.sh        \
+    quasardbTestbox.json         \
+    quasardbTestbox.motd.txt     \
+    quasardbTestbox.setup.sh     \
+    storageAccount.existing.json \
+    storageAccount.new.json      \
     virtualNetwork.json

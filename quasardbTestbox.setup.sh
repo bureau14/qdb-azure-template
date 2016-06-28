7f6b9ecb9b6c7b8b9d96a5a60089ab7eca56aa89
Benoit Blanchon <benoit@quasardb.net>
2016-06-28 18:19:58 +0200

    Switch to offer "quasardb", with SKUs "node" and "testbox"

--- quasardbTestbox.setup.sh
+++ quasardbTestbox.setup.sh
@@ -2,20 +2,8 @@
 
 echo "Invoked with args: $*"
 
-cd $(dirname $0)
-
-apt-get update
-apt-get -y install 'python-setuptools' 'php-pear' 'php5-dev' 'libpcre3-dev'
-dpkg -i *.deb
-tar xvf qdb-benchmark-*.tar.gz -C /usr
-easy_install *.egg
-pecl install quasardb*.tgz
-echo "extension=quasardb.so" > /etc/php5/cli/conf.d/quasardb.ini
-
 service qdb_httpd stop
 mv /etc/qdb/qdb_httpd.conf /etc/qdb/qdb_httpd.conf.orig
 qdb_httpd --gen-config --config=/etc/qdb/qdb_httpd.conf.orig $* > /etc/qdb/qdb_httpd.conf
 rm /etc/qdb/qdb_httpd.conf.orig
 service qdb_httpd start
-
-mv -f 'quasardbTestbox.motd.txt' '/etc/motd'

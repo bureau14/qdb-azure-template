66d34b62b4efc97d2f554a36c46913d29ec48a2d
Benoit Blanchon <benoit@quasardb.net>
2016-09-07 15:20:34 +0200

    Remove server log

--- quasardbNode.setup.sh
+++ quasardbNode.setup.sh
@@ -3,7 +3,9 @@
 echo "Invoked with args: $*"
 
 service qdbd stop
+sleep 5
 mv /etc/qdb/qdbd.conf /etc/qdb/qdbd.conf.orig
 qdbd --gen-config --config=/etc/qdb/qdbd.conf.orig $* > /etc/qdb/qdbd.conf
-rm /etc/qdb/qdbd.conf.orig
+rm -f /etc/qdb/qdbd.conf.orig
+rm -f /var/log/qdb/*
 service qdbd start
--- quasardbTestbox.setup.sh
+++ quasardbTestbox.setup.sh
@@ -3,7 +3,9 @@
 echo "Invoked with args: $*"
 
 service qdb_httpd stop
+sleep 5
 mv /etc/qdb/qdb_httpd.conf /etc/qdb/qdb_httpd.conf.orig
 qdb_httpd --gen-config --config=/etc/qdb/qdb_httpd.conf.orig $* > /etc/qdb/qdb_httpd.conf
-rm /etc/qdb/qdb_httpd.conf.orig
+rm -f /etc/qdb/qdb_httpd.conf.orig
+rm -f /var/log/qdb_http/*
 service qdb_httpd start
